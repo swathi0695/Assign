ce50e344ae0b9e85cd2f0e155f9d9cb81b42b7e7
swathi0695 <37923510+swathi0695@users.noreply.github.com>
2018-04-11 00:37:40 +0530

    Update Apr10_5.py

--- Apr10_5.py
+++ Apr10_5.py
@@ -1,9 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Apr 10 22:55:15 2018
 
-@author: Shyamala
-"""
 
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -12,7 +7,7 @@ from statistics import mode
 from collections import Counter
 
 #reading the file 
-df=pd.read_excel('C:\\Users\\Shyamala\\.spyder-py3\\Book1.xlsx')
+df=pd.read_excel('C:\\Users\\user\\.spyder-py3\\Book1.xlsx')
 print(df.describe())
 unit_price=df['Unit price']
 meanx=np.mean(unit_price)
